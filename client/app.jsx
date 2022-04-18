82986d9de136a8d0792c7df9500729e2827c10d0
justintaeson <justintaeson@gmail.com>
2022-04-18 20:13:41 +0000

    Updated app to direct users to login if they haven't been authorized.

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import Home from './pages/home';
 import Login from './pages/login';
+import Cookie from 'js-cookie';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isAuthorizing: true };
+  }
+
+  componentDidMount() {
+    const accessToken = Cookie.get('access_token');
+    if (accessToken === undefined) {
+      window.location.hash = '#login';
+    }
+    this.setState({
+      isAuthorizing: false
+    });
+  }
 
   render() {
+    if (this.state.isAuthorizing === true) {
+      return null;
+    }
     if (window.location.hash === '') {
       return <Home/>;
     }

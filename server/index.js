15df6682364108ac480d592daf5a08c098184ef4
justintaeson <justintaeson@gmail.com>
2022-04-22 23:31:30 +0000

    Tested with HTTPie + created a route for getting top tracks & artists in the last 6 months.

--- server/index.js
+++ server/index.js
@@ -93,8 +93,52 @@ app.get('/api/tracksalltime', refreshToken, (req, res, next) => {
     });
 });
 
+app.get('/api/tracks6months', refreshToken, (req, res, next) => {
+  fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=10', {
+    headers: {
+      Authorization: 'Bearer ' + req.user.access_token
+    }
+  })
+    .then(res => res.json())
+    .then(topTracks => {
+      const tracksArray = [];
+      for (let i = 0; i < topTracks.items.length; i++) {
+        tracksArray.push({
+          id: i,
+          artist: topTracks.items[i].artists[0].name,
+          track: topTracks.items[i].name,
+          image: topTracks.items[i].album.images[0].url,
+          popularity: topTracks.items[i].popularity
+        });
+      }
+      res.send(tracksArray);
+    });
+});
+
 app.get('/api/artistsalltime', refreshToken, (req, res, next) => {
   fetch('https://api.spotify.com/v1/me/top/artists?time_range=long_term&limit=10', {
+    headers: {
+      Authorization: 'Bearer ' + req.user.access_token
+    }
+  })
+    .then(res => res.json())
+    .then(topArtists => {
+      const artistsArray = [];
+      for (let i = 0; i < topArtists.items.length; i++) {
+        artistsArray.push({
+          id: i,
+          artist: topArtists.items[i].name,
+          popularity: topArtists.items[i].popularity,
+          image: topArtists.items[i].images[0].url,
+          genre: topArtists.items[i].genres[0]
+        });
+      }
+      res.send(artistsArray);
+    });
+});
+
+app.get('/api/artists6months', refreshToken, (req, res, next) => {
+  fetch('https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=10', {
     headers: {
       Authorization: 'Bearer ' + req.user.access_token
     }
